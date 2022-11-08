a03670894c74071dd57e8c2485f745e8e2447890
RhazzXIX <jm21tonio@gmail.com>
2022-11-08 13:59:37 +0800

    Finnish 01-04 java excercises
    01 learned how to test via npm and familiarize on the results of
    the tests
    02 learned how to add/repeat strings
    03 learned how to create split strings and create new strings
    04 learned how to process multiple arguments in a java functions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+const reverseString = function(string) {
+// get string length for repetetion
+let num = string.length;
+// variable for array
+const revArray = [];
+// variable to return
+let result = '';
 
+// loop and splicing
+for (i = (num -1 ); i >= 0; i--) {
+    revArray.push(string.substr(i,1));
+};
+// loop for joining
+for (j = 0; j < num; j++) {
+    result += revArray[j];
+}
+
+return result;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(numArray,num,...toRemove) {
+    
+    toRemove.push(num);  
+    toRemove.sort();
 
-};
+    for(let i = 0; i <= numArray.length - 1; i++){
+      for (let j = 0; j <= toRemove.length -1; j++) {
+        if (numArray[i] === toRemove[j]) {
+            numArray.splice(i,1);
+        } else { continue;}
+      }
+    }
 
+
+    
+    return numArray;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
