5d7ee319d213b43ded1b2c8054ae75f45cb8874e
RhazzXIX <jm21tonio@gmail.com>
2022-11-09 12:22:25 +0800

    Finnish 05-07 java exercises
    Learned to use properties and methods after looking at soultions to
    shorten the formula.
    Refreshed learning about if(){}, that it can end without any else{}.
    Solidified learnings about the sequence of initialization of a code
    structure.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
 const sumAll = function() {
+    // get arguments and sort them
+    let numArg = Array.from(arguments);
+        numArg = numArg.sort();  
+    // sum holder & variable to add
+    let sum;
+    let add = 0;
+    // increments the number from first argument then adds them
+    // until the last arguments
+    for(let i = numArg[0]; i <= numArg[numArg.length -1]; i++) {
+            sum = add += i;   
+    };
+    // check array if NAN || negative == ERROR
+    for(let j = 0; j <= 1; j++) {
+        if (numArg[j] < 0 || typeof numArg[j] != typeof 1) {
+            sum = 'ERROR'; break;
+        }; 
+    };
+    
 
-};
+    return sum;
+}; 
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  // conversion to celsius
+  let celcius = ((num-32)*(5/9));
+  // Round to single decimal without affecting interger results
+  // Multiply the result to 10 for the decimal to avoid the Math.Round
+  // Math.round then round the number to the nearest integer
+  // Divide the result by 10 to return the decimal to the float.
+  celcius = (Math.round(celcius * 10)) / 10;
+  return celcius;
 };
 
-const ctof = function() {
+const ctof = function(num) {
+  // conversion to farenheit
+  let farenheit = ((num*(9/5))+32);
+  // round to single float without affecting integer results
+  // Multiply the result to 10 for the decimal to avoid the Math.Round
+  // Math.round then round the number to the nearest integer
+  // Divide the result by 10 to return the decimal to the float.
+  farenheit = (Math.round(farenheit * 10)) / 10;
+  return farenheit;
 
 };
 
