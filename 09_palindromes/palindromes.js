06ffe9f085d15680ee76467d877f3f42e08dd5a7
RhazzXIX <jm21tonio@gmail.com>
2022-11-18 18:53:24 +0800

    Finnish java exercises 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,47 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //lowecase the string
+    let lowString = string.toLowerCase();
+    
+    //create the string into array
+    let arrString = lowString.split('');
+
+    //removes the punctuations
+    let clearArray = clear(arrString);
+
+    
+    function clear(array) {
+        let toRemove = ['!', "'", '?', ' ','.',',']
+        for(let x = 0; x < 2; x++){
+            for ( let i = 0; i <= toRemove.length; i++) {
+                for (let j = 0; j <= array.length; j++) {
+                    if (array[j] === toRemove[i]) {
+                        array.splice(j,1);
+                    };
+                };
+            };
+        };   
+        return array;
+    }
+    //reverse the array
+    function reverse (array) {
+        let newArray =[];
+        for(let i = 0; i < array.length; i++){
+            newArray.push(array[i]);
+        };  return newArray.reverse();
+    }
+    
+    let revArray = reverse(clearArray);
+
+    //combines the array
+
+    let clearString = clearArray.join('');
+    let reverseString = revArray.join('');
+    console.log(clearString);
+    console.log(reverseString);
+
+    if(clearString === reverseString) {
+        return true;
+    } else {return false};
 
 };
 
