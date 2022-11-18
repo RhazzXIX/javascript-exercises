e05ce91a98eaf85fba695fa537ce93f5085c711a
RhazzXIX <jm21tonio@gmail.com>
2022-11-18 14:24:31 +0800

    Finnish java exercises 08_calculator.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
+const add = function(a, b) {
+  let sum = Math.round((a + b) * 10) / 10;
+  return sum;
+};
+
+const subtract = function(a, b) {
+  let diff = Math.round((a - b) * 10) / 10;
+  return diff;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for(let i = 0; i < array.length; i++) {
+    sum += array[i];
+  };
+  return sum;
 };
 
-const sum = function() {
-	
+const multiply = function(multi) {
+  let prod = 1;
+  for(let i = 0; i < multi.length; i++) {
+    prod *= multi[i];
+  };
+  return prod;
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+  let power = 1;
+  for(let i = 0; i < b; i++) {
+    power *= a;
+  };
+	return power;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factor = 1;
+  if(num == 0) {return 1};
+  for(let i = 1; i <= num; i++) {
+    factor *= i;
+  }
+	return factor;
 };
 
 // Do not edit below this line
