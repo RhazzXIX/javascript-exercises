7cebaaa8e8edb44675bd4790d618dbd956c23515
RhazzXIX <jm21tonio@gmail.com>
2022-11-19 09:55:11 +0800

    Finnish javascript exercises 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibo = 0,
+        first = 0;
+        second = 1;
+        if(num < 0) {
+            return 'OOPS'
+        } else { 
+            let i = 1
+            do {fibo = first + second;
+                first = second;
+                second = fibo;
+                i++;
+            } while (i < num);
+            // for(i = 1; i < num; i++) {
+            //     fibo = first + second;
+            //     first = second;
+            //     second = fibo;
+            // };
+        };
 
+    return fibo;
 };
 
 // Do not edit below this line
